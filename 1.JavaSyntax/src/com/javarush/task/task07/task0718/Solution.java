97f78f41f36beb132a3111b23a554f1040243c30
bobark <bobark@adex.ru>
2022-01-05 10:17:46 +0700

    Task 0718

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task07.task0718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Проверка на упорядоченность
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+        int lenght = list.get(0).length();
+        for (int i = 0; i < list.size(); i++) {
+            int m = list.get(i).length();
+            if (lenght>m) {
+                System.out.println(i);
+                break;
+            }
+            else
+                lenght = list.get(i).length();
+
+        }
+    }
+}
+
