e5c5e8a283f1d14736bcdac602afaef9708fda0e
bobark <bobark@adex.ru>
2022-01-06 10:20:40 +0700

    Task 0727

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0727/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task07.task0727;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Меняем функциональность
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> strings = new ArrayList<String>();
+        while (true) {
+            String string = reader.readLine();
+            if (string == null || string.isEmpty()) break;
+            strings.add(string);
+        }
+
+        ArrayList<String> resultStrings = new ArrayList<String>();
+        for (int i = 0; i < strings.size(); i++) {
+            String string = strings.get(i);
+           if (string.length()%2 == 0){
+              String tmp = string + " " + string;
+               resultStrings.add(tmp);
+           }
+           else {
+               String tmp = string + " " + string + " " + string;
+               resultStrings.add(tmp);
+           }
+
+
+        }
+
+        for (int i = 0; i < resultStrings.size(); i++) {
+            System.out.println(resultStrings.get(i));
+        }
+    }
+}
