a1f62be9c1d4a5f68ab7e1ccb94718929f803191
bobark <bobark@adex.ru>
2022-01-06 10:30:51 +0700

    Task 0813 Set

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        Set<String> set = new HashSet<String>();
+        set.add("Люба1");
+        set.add("Люба2");
+        set.add("Люба3");
+        set.add("Люба4");
+        set.add("Люба5");
+        set.add("Люба6");
+        set.add("Люба7");
+        set.add("Люба8");
+        set.add("Люба9");
+        set.add("Люба10");
+        set.add("Люба11");
+        set.add("Люба12");
+        set.add("Люба13");
+        set.add("Люба14");
+        set.add("Люба15");
+        set.add("Люба16");
+        set.add("Люба17");
+        set.add("Люба18");
+        set.add("Люба19");
+        set.add("Люба20");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
