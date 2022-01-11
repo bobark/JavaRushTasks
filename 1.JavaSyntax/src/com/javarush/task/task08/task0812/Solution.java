1927822d3348e795b44bfcab456a2c52bed47c90
bobark <bobark@adex.ru>
2022-01-11 16:24:47 +0700

    Task 0812

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Cамая длинная последовательность
@@ -12,24 +13,28 @@ Cамая длинная последовательность
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list = new ArrayList<>();
+      List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             list.add(Integer.parseInt(reader.readLine()));
         }
         int q = 1;
-        int max = -1;
+        int max = 1;
         for (int i = 0; i < list.size() - 1; i++) {
             if (list.get(i).equals(list.get(i + 1))) {
-                q++;
-                if (max < q) {
-                    max = q;
-
+                max++;
+            }
+            else if (max > q) {
+                    q = max;
+                    max = 1;
                 }
-            } else {
-                q = 1;
+             else {
+                max = 1;
             }
         }
+        if (max > q) {
+            q = max;
+        }
 
-        System.out.println(max);
+        System.out.println(q);
     }
 }
