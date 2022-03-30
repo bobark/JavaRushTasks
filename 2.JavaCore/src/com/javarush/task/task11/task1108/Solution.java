9a98bdd364c439bebef4a1257b183cc7111abd5a
bobark <bobark@adex.ru>
2022-03-30 14:24:22 +0700

    Java Core 1 level 1108 task

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1108/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task11.task1108;
+
+/* 
+Неприступный кот
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        private void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        private void setAge(int age) {this.age = age;}
+
+        public int getWeight() {return weight; }
+
+        private void setWeight(int weight) {this.weight = weight;}
+    }
+}
